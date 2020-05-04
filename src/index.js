c0b220305fd4136b10176284fae5d7c6002339f1
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-05-04 11:01:18 -0700

    basic size and topping options

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './style.scss';
-import {pizza} from './pizza.js';
-var toppingButtons = document.getElementsByClassName("toppingButton");
+import {
+    pizza
+} from './pizza.js';
+var toppingButtons = document.getElementsByClassName("innerAmountButton");
+var sizeButtons = document.getElementsByClassName("sizeButton");
+var size = "personal"
+document.getElementById("size").innerHTML = size;
+
 
 document.addEventListener('DOMContentLoaded', () => {
     for (let index = 0; index < toppingButtons.length; index++) {
@@ -9,14 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
             addTopping(element)
         }
     }
+    for (let index = 0; index < sizeButtons.length; index++) {
+        const element = sizeButtons[index];
+        element.onclick = () => {
+            changeSize(element);
+        }
+    }
 })
 
 const addTopping = (button) => {
-    var value = button.attributes.value.nodeValue;
-    // console.log(value);
+    var topping = button.parentNode.parentNode.attributes.value.nodeValue;
+    var side = button.parentNode.attributes.value.nodeValue;
+    var amount = button.attributes.value.nodeValue;
+    var image = pizza.toppings[topping].images[size][side][amount]
+    document.getElementById("list").innerHTML = document.getElementById("list").innerHTML.concat("<br>" + pizza.toppings[topping].name + " " + side + " " + amount);
+    document.getElementById("test").innerHTML = "<img src=\"" + image + "\">"
+}
 
-    var testDiv = document.createElement("div");
-    testDiv.innerHTML = "<img src=\""+pizza.toppings.Chicken.images.xlarge.left.regular+"\">"   
-    document.getElementById("test").appendChild(testDiv);
-    // console.log(pizza.toppings.Chicken.images.xlarge.left);
+const changeSize = (button) => {
+    var sizeVal = button.attributes.value.nodeValue;
+    size = sizeVal;
+    document.getElementById("size").innerHTML = size;
 }
