72782727882028d7cb5e0a5ebaa15ef6a3d3612f
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-05-06 08:10:50 -0700

    canvas testing

--- src/index.js
+++ src/index.js
@@ -7,19 +7,34 @@ var sizeButtons = document.getElementsByClassName("sizeButton");
 var size = "personal"
 document.getElementById("size").innerHTML = size;
 
+var c = document.getElementById("myCanvas");
+var img = document.createElement('IMG');
+img.src = pizza.toppings.Bacon.images.large.full.extra;
+img.style.display = "none";
+document.getElementById('pizza').appendChild(img)
+
+
+img.onload = () =>{
+    var ctx = c.getContext("2d");
+    console.log(img)
+    ctx.drawImage(img, 50,50,100,100);
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    //topping buttons
     for (let index = 0; index < toppingButtons.length; index++) {
         const element = toppingButtons[index];
         element.onclick = () => {
             addTopping(element)
         }
     }
+
+    //size buttons
     for (let index = 0; index < sizeButtons.length; index++) {
         const element = sizeButtons[index];
         element.onclick = () => {
             changeSize(element);
-            
         }
     }
 })
@@ -31,9 +46,11 @@ const addTopping = (button) => {
     var image = pizza.toppings[topping].images[size][side][amount]
     
     if (button.classList.contains('selected')){
+        //remove
         button.classList.remove('selected')
         document.getElementById("list").innerHTML = document.getElementById("list").innerHTML.replace("<br>" + pizza.toppings[topping].name + " " + side + " " + amount, "");
-    }else{   
+    }else{  
+        //add 
         button.classList.add('selected');
         document.getElementById("list").innerHTML = document.getElementById("list").innerHTML.concat("<br>" + pizza.toppings[topping].name + " " + side + " " + amount);
         document.getElementById("test").innerHTML = "<img src=\"" + image + "\">"
